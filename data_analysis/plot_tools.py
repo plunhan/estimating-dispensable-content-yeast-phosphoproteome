99b90c67ef8a0c8e5250c81c7bcaca822e1d617c
Yilun Han <plunhan@yiluns-imac.home>
2025-06-27 14:44:47 -0400

    Add scripts and helpers for plotting distribution of p-sites.
    Include scripts and helpers for plotting distribution of p-sites
    count against the number of perturbations where the p-sites are
    detected, in ordered and disordered regions, respectively.

---
+++ data_analysis/plot_tools.py
@@ -0,0 +1,159 @@
+from pathlib import Path
+from typing import Union
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from scipy.stats import ranksums
+from statannotations.Annotator import Annotator
+
+def significance_level(p_value: float) -> str:
+    if p_value < 0.0001:
+        return '****'
+    elif p_value < 0.001:
+        return '***'
+    elif p_value < 0.01:
+        return "**"
+    elif p_value < 0.05:
+        return "*"
+    else:
+        return "n.s."
+
+def plot_count_per_perturbation(count_d: dict[int, set[str]], 
+                                outPath: Union[str, Path], 
+                                title: str,
+                                fmt: str) -> None:
+    '''
+    Plot count of phosphosites against the number of perturbations the phosphosite is detected. 
+
+    Args:
+        count_d (dict[int, set[str]]): A dictionary mapping the number of perturbations to phosphosite references.
+        outPath (Union[str, Path]): Path to the output file for the plot. 
+        fmt (str): Output file format. 
+
+    Returns:
+        None
+    '''
+    x = sorted(count_d.keys())
+    y = [len(count_d[number]) for number in x]
+    plt.figure(figsize=(6, 4))
+    plt.bar(x, y, color='skyblue')
+    plt.xlabel('Number of perturbations in which a phosphosite is detected')
+    plt.ylabel('Count of phosphosites')
+    plt.title(title)
+    plt.savefig(outPath, format=fmt, dpi=300)
+    plt.close()
+
+def plot_comparison_ConSurf(consurf_1: list[float],
+                            consurf_2: list[float],
+                            label_1: str,
+                            label_2: str,
+                            outPath: Union[str, Path],
+                            fmt: str) -> None:
+    '''
+    Plot comparison between two groups of ConSurf scores. 
+
+    Args:
+        consurf_1 (list[float]): ConSurf scores of the first group. 
+        consurf_2 (list[float]): ConSurf scores of the second group.
+        label_1 (str): Label of the first group. 
+        label_2 (str): Label fo the second group. 
+        outPath (Union[str, Path]): Path to the output file for the plot. 
+        fmt (str): Output file format. 
+
+    Returns:
+        None
+    '''
+    data = consurf_1 + consurf_2
+    labels = [label_1] * len(consurf_1) + [label_2] * len(consurf_2)
+
+    plt.figure(figsize=(6, 5))
+    ax = sns.boxplot(x=labels, y=data, showfliers=False)
+    plt.xlabel('Group of phosphosites')
+    plt.ylabel('ConSurf score')
+    y_max = max(data) + 0.1
+    x1, x2 = 0, 1
+    plt.plot([x1, x1, x2, x2], 
+             [y_max, y_max + 0.05, y_max + 0.05, y_max],
+             lw=1.5, 
+             c='black')
+    stat, p_value = ranksums(consurf_1, consurf_2)
+    sig = significance_level(p_value)
+    plt.text((x1 + x2) / 2,
+             y_max + 0.07,
+             sig,
+             ha='center', 
+             va='bottom',
+             fontsize=12)
+    plt.tight_layout()
+    plt.savefig(outPath, format=fmt, dpi=300)
+    plt.close()
+
+def plot_distribution_consurf(consurf_ls_ls: list[list[float]], 
+                              outPath: Union[str, Path], 
+                              fmt: str, 
+                              label_list: list[str]) -> None: 
+    all_data = np.concatenate(consurf_ls_ls)
+    x_min, x_max = min(all_data), max(all_data)
+
+    num_bins = 20
+    shared_bins = np.linspace(x_min, x_max, num_bins + 1)
+
+    y_max = 0
+    for consurf_ls in consurf_ls_ls:
+        counts, bins = np.histogram(consurf_ls, bins=20, density=True)
+        y_max = max(y_max, max(counts))
+    
+    fig, axes = plt.subplots(nrows=len(consurf_ls_ls), ncols=1, figsize=(6, 4 * len(consurf_ls_ls)))
+
+    if len(consurf_ls_ls) == 1:
+        axes = [axes]
+
+    for i, consurf_ls in enumerate(consurf_ls_ls):
+        axes[i].hist(consurf_ls, bins=shared_bins, density=True, edgecolor='black')
+        axes[i].set_title(f'{label_list[i]}')
+        axes[i].set_xlim(x_min, x_max)
+        axes[i].set_ylim(0, y_max * 1.1)
+        axes[i].set_ylabel('Probability density')
+    plt.xlabel('ConSurf score')
+    plt.savefig(outPath, format=fmt, dpi=300)
+    plt.close()
+'''
+def plot_figure2b(ax, data, title, labels):
+    sns.boxplot(data=data, ax=ax, showfliers=False)
+    ax.set_title(title)
+    ax.set_xticklabels(labels, rotation=0)
+
+    y_max = max(max(group) for group in data) + 0.1
+    step = 0.05
+    count = 0
+
+    for i in range(len(data)):
+        for j in range(i+1, len(data)):
+            stat, p = ranksums(data[i], data[i+1])
+            label = f'p={p:.2e}'
+            x1, x2 = i, j
+            y = y_max + count * step
+            ax.plot([x1, x1, x2, x2], [y, y+0.02, y+0.02, y], lw=1.2, c='k')
+            ax.text((x1 + x2) * 0.5, y + 0.025, label, ha='center', va='bottom', fontsize=9)
+            count += 1
+'''
+def melt_data(data, group_labels, ylabel):
+    flat_values = sum(data, [])
+    group_column = [label for label, group in zip(group_labels, data) for _ in group]
+    return pd.DataFrame({'Group': group_column, ylabel: flat_values})
+
+def plot_figure2b(ax, data, title, pairs, labels, ylabel):
+    df = melt_data(data, labels, ylabel)
+    sns.boxplot(x='Group', y=ylabel, data=df, ax=ax, showfliers=False)
+    ax.set_title(title)
+
+    # max_value = df['Value'].max()
+    # y_max = max_value + 0.3 * (max_value - df['Value'].min())
+    # ax.set_ylim(top=y_max)
+    ax.set_ylim(bottom=df[ylabel].min()+0.5)
+    
+    annotator = Annotator(ax, pairs, data=df, x='Group', y=ylabel)
+    annotator.configure(test='Mann-Whitney', text_format='star', loc='inside')
+    annotator.apply_and_annotate()
---
+++ data_analysis/psite_count_distribution.py
@@ -0,0 +1,57 @@
+# Plot Figures 1A and 1B
+
+import os
+import pickle
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import ranksums, ttest_ind
+
+from plot_tools import plot_count_per_perturbation
+from proteomic_tools import retrieve_references_by_residue_type, retrieve_references_by_order
+
+def main():
+
+    resType = 'ST'
+
+    figFmt = 'jpg'
+
+    dataDir = Path('../../data')
+
+    procDir = dataDir / 'processed'
+
+    paperDir = procDir / 'paper'
+
+    # input files
+    ultradeepPKL = paperDir / 'ultradeep_reference_phosphoproteome.pkl'
+    phosStresPKL = paperDir / 'quantitative_phosphosites.pkl'
+    consurfPKL = paperDir / 'consurf_all.pkl'
+    disoPKL = paperDir / 'diso_all.pkl'
+    sequencePKL = paperDir / 'Scer_seq.pkl'
+    sgdPKL = paperDir / 'SGD.pkl'
+    biogridPKL = paperDir / 'BioGRID.pkl'
+
+    # output files
+    Fig1A = paperDir / 'Figure 1A.jpg'
+    Fig1B = paperDir / 'Figure 1B.jpg'
+
+    diso = pickle.load(open(disoPKL, 'rb'))
+    phosStres = pickle.load(open(phosStresPKL, 'rb'))
+    phosStres = {key: retrieve_references_by_residue_type(references, resType) for key, references in phosStres.items()}
+    phosStres_ord = {key: retrieve_references_by_order(references, diso, 'ordered') for key, references in phosStres.items()}
+    phosStres_dis = {key: retrieve_references_by_order(references, diso, 'disordered') for key, references in phosStres.items()}
+
+    if not Fig1A.is_file(): 
+        plot_count_per_perturbation(phosStres_ord, 
+                                    Fig1A, 
+                                    'Detection frequency of phosphosites in ordered\nregions across perturbations',
+                                    figFmt)
+    if not Fig1B.is_file(): 
+        plot_count_per_perturbation(phosStres_dis, 
+                                    Fig1B, 
+                                    'Detection frequency of phosphosites in disordered\nregions across perturbations',
+                                    figFmt)
+
+if __name__ == '__main__':
+    main()
